da82bf59ab592ca43f78eba6c9328798b51090f3
Duy Khoa <duykhoa1125@gmail.com>
2025-09-28 07:52:48 +0700

    add more

--- config/db.js
+++ config/db.js
@@ -3,33 +3,36 @@ import mongoose from "mongoose";
 let cached = global.mongoose;
 
 if (!cached) {
-    //check if global.mongoose is undefined, if so, initialize it
+  //check if global.mongoose is undefined, if so, initialize it
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-    if(cached.conn) {
-        console.log("Using cached MongoDB connection");
-        return cached.conn;
-    }
-
-    if(!cached.promise) {
-        const opts = {
-          // (tắt tính năng mongoose giữ lệnh khi chưa có kết nối).
-          bufferCommands: false,
-        };
-
-        console.log("Connecting to MongoDB...");
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then((mongoose)=> {
-            console.log("MongoDB connected successfully");
-            return mongoose;
-        }).catch((error) => {
-            console.error("MongoDB connection error:", error);
-            throw error;
-        })
-    }
-    cached.conn = await cached.promise;
+  if (cached.conn) {
+    console.log("Using cached MongoDB connection");
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      // (tắt tính năng mongoose giữ lệnh khi chưa có kết nối).
+      bufferCommands: false,
+    };
+
+    console.log("Connecting to MongoDB...");
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+      .then((mongoose) => {
+        console.log("MongoDB connected successfully");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        throw error;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectDB;
